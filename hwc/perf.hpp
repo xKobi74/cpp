d26f20c3bc4c821c84b2e3df7a6172458ec35957
Evgeniy Kobi <bogdanov.es@phystech.edu>
2022-09-13 18:04:41 +0300

    Perf_alg_t optimisation

--- hwc/perf.hpp
+++ hwc/perf.hpp
@@ -13,9 +13,24 @@ template <typename KeyT> struct perf_alg_t {
 	std::set<KeyT> set;
 	std::unordered_map<KeyT, std::queue<KeyT>> hashmap;
 	int hits;
+	KeyT nothing;
+	struct pair_t {
+		int ind;
+		KeyT key;
 
-	perf_alg_t(int cap, std::vector<KeyT> &input) : capacity(cap), hits(0) {
-		assert(cap > 0);
+		pair_t(int i, KeyT k) : ind(i), key(k) {}
+		void equal(struct pair_t &p) {
+			this->ind = p.ind;
+			this->key = p.key;
+		}
+		bool less(struct pair_t p) {
+			return (this->ind) < (p.ind);
+		}
+	};
+	struct pair_t first, second; 
+
+	perf_alg_t(int cap, std::vector<KeyT> &input, KeyT noth) : capacity(cap), hits(0), first(0, noth), second(0, noth), nothing(noth) {
+		assert(cap > 1);
 		int requestscount = input.size();
 		int key;
 		for (int i = 0; i < requestscount; ++i) {
@@ -42,24 +57,39 @@ template <typename KeyT> struct perf_alg_t {
 			++hits;
 			return;
 		}
+		int ind = nextplace(key);
+		struct pair_t pair(ind, key); 
 		if (isfull()) {
-			KeyT max = *(set.begin()), el;
-			int imax = nextplace(max), iit;
-			for (auto it = ++(set.begin()); it != set.end(); ++it) {
-				el = *it;
-				iit = nextplace(el);
-				if (iit > imax) {
-					imax = iit;
-					max = el;
-				}
+			set.erase(first.key);
+			set.insert(key);
+			if (second.less(pair))
+				first.equal(pair);
+			else {
+				first.equal(second);
+				second.equal(first);
 			}
-			if (max == key) {
-				++hits;
-				return;
-			}
-			set.erase(max);
+			return;
 		}
 		set.insert(key);
+		if (first.key == nothing)
+			first.equal(pair);
+		else {
+			if (second.key == nothing) {
+				if (first.less(pair)) {
+					second.equal(first);
+					first.equal(pair);
+				}
+				else
+					second.equal(pair);
+			}
+			if (first.less(pair)) {
+				second.equal(first);
+				first.equal(pair);
+			} 
+			else 
+				if (second.less(pair))
+					second.equal(pair);
+		}
 	}
 
 	int hitscount() const {
